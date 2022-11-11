e16517642b325a812b20e2d023551c5c96d8d795
Victor Seva <linuxmaniac@torreviejawireless.org>
2022-11-11 10:18:45 +0100

    fix entrypoint after ed78421cd0411ba85b59396e83cb3d893f3a12cb
    (cherry picked from commit 7df12009fbfd896c9fdad50de73833bbc53c399d)

--- create_dockerfile.sh
+++ create_dockerfile.sh
@@ -50,10 +50,10 @@ VOLUME /etc/kamailio
 RUN apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # set SHM_MEMORY and PKG_MEMORY from ENV
-ENV SHM_MEMORY=${SHM_MEMORY:-64}
-ENV PKG_MEMORY=${PKG_MEMORY:-8}
+ENV SHM_MEMORY=\${SHM_MEMORY:-64}
+ENV PKG_MEMORY=\${PKG_MEMORY:-8}
 
-ENTRYPOINT kamailio -DD -E -m $SHM_MEMORY -M $PKG_MEMORY
+ENTRYPOINT ["kamailio", "-DD", "-E", "-m \${SHM_MEMORY}", "-M \${PKG_MEMORY}"]
 EOF
 }
 
