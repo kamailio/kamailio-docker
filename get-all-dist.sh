acb7fa176b9fecadbb534e2a66d8b4e76d91a36d
Victor Seva <linuxmaniac@torreviejawireless.org>
2024-02-06 09:02:57 +0100

    github: allow manual triggering of selected distributions [skip ci]
    (cherry picked from commit e62bd173bda49c1d60e6c54e39d8707a19a93b92)

--- get-all-dist.sh
+++ get-all-dist.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-find . -name Dockerfile -exec dirname {} \; | \
-  grep -v -e 'stretch' -e 'bookworm' | \
-  jq -R . | sed 's#./##g' | jq -cs .
+if [ -n "${1}" ] ; then
+  echo "\"${1}\"" | jq -c 'split(",")'
+else
+  find . -name Dockerfile -exec dirname {} \; | \
+    grep -v -e 'stretch' | \
+    jq -R . | sed 's#./##g' | jq -cs .
+fi
