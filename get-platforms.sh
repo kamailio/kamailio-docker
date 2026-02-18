50183615761d17457643bc03f8b266e973eae509
Victor Seva <linuxmaniac@torreviejawireless.org>
2026-02-18 20:28:57 +0100

    github: dynamic supported plataforms

---
+++ get-platforms.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+supported=('linux/amd64')
+if [[ ${GITHUB_REF:-} =~ refs/heads/([5-9])\.([0-9]+)\.[0-9]+ ]] ; then
+  if [[ "${BASH_REMATCH[1]}" -ge 6 ]] && [[ "${BASH_REMATCH[2]}" -ge 1 ]] ; then
+    supported+=('linux/arm64')
+  fi
+fi
+echo "${supported[*]}" | jq -R . | jq -c 'split(" ")'
